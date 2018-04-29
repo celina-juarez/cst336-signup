d3e5cc5360d570c4297f997e103c40b49dd17c07
Celina Juarez <cejuarez@csumb.edu>
2018-04-29 20:56:41 +0000

    Changed variable names

--- dbConnection.php
+++ dbConnection.php
@@ -11,25 +11,20 @@ function connectToDB($dbName) {
     if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
        $host = $url["host"];
-       $dbname   = substr($url["path"], 1);
-       $username = $url["user"];
-       $password = $url["pass"];
+       $db   = substr($url["path"], 1);
+       $user = $url["user"];
+       $pass = $url["pass"];
     }
     
-    /*$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
     $opt = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
     ];
     $pdo = new PDO($dsn, $user, $pass, $opt);
-    return $pdo; */
+    return $pdo; 
     
-    // Create connection
-    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    return $dbConn;
 }
 
 
