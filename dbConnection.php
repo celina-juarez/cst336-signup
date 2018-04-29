6543313ac868e5796c05642de9d92e5b9d4d0348
Celina Juarez <cejuarez@csumb.edu>
2018-04-29 20:52:43 +0000

    HELP

--- dbConnection.php
+++ dbConnection.php
@@ -16,16 +16,21 @@ function connectToDB($dbName) {
        $password = $url["pass"];
     }
     
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    /*$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
     $opt = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
     ];
     $pdo = new PDO($dsn, $user, $pass, $opt);
-    return $pdo; 
-}
+    return $pdo; */
+    
+    // Create connection
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    return $dbConn;
+}
 
 
 
